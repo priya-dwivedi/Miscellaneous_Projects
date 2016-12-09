f502a31c4d1f4fb67f35680e2f92f2dc154c4a6d
Priyanka Dwivedi <priyanka.kochhar3@gmail.com>
2016-12-09 12:39:46 -0500

    Add files via upload

---
+++ RNN/convert_shk_lowercase.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 11:53:44 2016
+
+@author: s6324900
+"""
+
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import os
+import numpy as np
+import random
+import string
+import zipfile
+from six.moves import range
+from six.moves.urllib.request import urlretrieve
+
+
+import os
+os.chdir(r"C:\Users\s6324900\Desktop\Deep learning\RNN")
+
+text = open('input.txt', 'r').read() # should be simple plain text file
+print('Data size %d' % len(text))
+
+#Create a small validation set
+valid_size = 1000
+valid_text = text[:valid_size]
+train_text = text[valid_size:]
+train_size = len(train_text)
+print(train_size, train_text[:64])
+print(valid_size, valid_text[:64])
+
+#char2id
+
+vocabulary_size = len(string.ascii_lowercase) + 3 # [a-z] + ' '
+first_letter = ord(string.ascii_lowercase[0])
+
+def char2id(char):
+  if char in string.ascii_lowercase:
+    return ord(char) - first_letter + 1
+  elif char in string.ascii_uppercase:
+    return ord(char) - first_letter + 33  
+  elif char == ' ':
+    return 0
+  elif char == '-':
+    return 27
+  elif char == ':':
+    return 27
+  elif char == ',':
+    return 28
+  elif char == ';':
+    return 28
+  else:
+    print('Unexpected character: %s' % char)
+    return 0
+  
+def id2char(dictid):
+  if dictid > 0:
+    return chr(dictid + first_letter - 1)
+  elif dictid == 27:
+    return(":")
+  elif dictid == 28:
+    return(",")
+  else:
+    return ' '
+
+print(char2id('a'), char2id('z'), char2id(' '), char2id('A'), char2id('Z'), char2id('ï'))
+print(id2char(1), id2char(26), id2char(0))
+
+##Check if it works
+batch_size=64
+num_unrollings=10
+
+class BatchGenerator(object):
+  def __init__(self, text, batch_size, num_unrollings):
+    self._text = text
+    self._text_size = len(text)
+    self._batch_size = batch_size
+    self._num_unrollings = num_unrollings
+    segment = self._text_size // batch_size
+    self._cursor = [ offset * segment for offset in range(batch_size)]
+    self._last_batch = self._next_batch()
+  
+  def _next_batch(self):
+    """Generate a single batch from the current cursor position in the data."""
+    batch = np.zeros(shape=(self._batch_size, vocabulary_size), dtype=np.float)
+    for b in range(self._batch_size):
+      batch[b, char2id(self._text[self._cursor[b]])] = 1.0
+      self._cursor[b] = (self._cursor[b] + 1) % self._text_size
+    return batch
+  
+  def next(self):
+    """Generate the next array of batches from the data. The array consists of
+    the last batch of the previous array, followed by num_unrollings new ones.
+    """
+    batches = [self._last_batch]
+    for step in range(self._num_unrollings):
+      batches.append(self._next_batch())
+    self._last_batch = batches[-1]
+    return batches
+
+def characters(probabilities):
+  """Turn a 1-hot encoding or a probability distribution over the possible
+  characters back into its (most likely) character representation."""
+  return [id2char(c) for c in np.argmax(probabilities, 1)]
+
+def batches2string(batches):
+  """Convert a sequence of batches back into their (most likely) string
+  representation."""
+  s = [''] * batches[0].shape[0]
+  for b in batches:
+    s = [''.join(x) for x in zip(s, characters(b))]
+  return s
+
+train_batches = BatchGenerator(train_text, batch_size, num_unrollings)
+valid_batches = BatchGenerator(valid_text, 1, 1)
+
+print(batches2string(train_batches.next()))
+print(batches2string(train_batches.next()))
+print(batches2string(valid_batches.next()))
+print(batches2string(valid_batches.next()))
